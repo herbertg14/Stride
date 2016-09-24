02047e1f6a13bf933b85c50e95cc9e3ac10c9b98
herbertg14 <herbert.gutierrez@utexas.edu>
2016-09-24 02:42:12 -0500

    User sign up created. No saving to Firebase yet.

--- app/src/main/java/com/example/herbert/stride/MainActivity.java
+++ app/src/main/java/com/example/herbert/stride/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.herbert.stride;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+//            firebaseAuth returns the results if a user is logged in or not
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+//                if the user is not signed in then send the user to the login activity and clear the top so the user won't be able to go back
+                if (firebaseAuth.getCurrentUser() == null){
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class );
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+                }
+            }
+        };
+    }
+
+//    create the menu tab
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
     }
 }
--- app/src/main/java/com/example/herbert/stride/SignUpActivity.java
+++ app/src/main/java/com/example/herbert/stride/SignUpActivity.java
@@ -2,12 +2,58 @@ package com.example.herbert.stride;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText mName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mRePassword;
+//
+    private Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mName = (EditText) findViewById(R.id.editTextName);
+        mEmail = (EditText) findViewById(R.id.editTextEmail);
+        mPassword = (EditText) findViewById(R.id.editTextPassword);
+        mRePassword = (EditText) findViewById(R.id.editTextPassword2);
+        mRegisterButton = (Button) findViewById(R.id.registerButton);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(SignUpActivity.this, "Button works", Toast.LENGTH_SHORT).show();
+
+                if (mPassword.getText().toString().trim() != mRePassword.getText().toString().trim()){
+                    Toast.makeText(SignUpActivity.this, "Password does not match", Toast.LENGTH_SHORT).show();
+                }
+                else{
+//                    Toast.makeText(SignUpActivity.this, "You did everything right", Toast.LENGTH_SHORT).show();
+                    startRegistration();
+                }
+            }
+
+            private void startRegistration() {
+                String name = mName.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+//                if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+//
+//                }
+
+            }
+        });
+
+
     }
 }
