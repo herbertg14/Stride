12a1447cbbaa926d32317b34838021449f23272e
herbertg14 <herbert.gutierrez@utexas.edu>
2016-09-24 01:02:14 -0500

    Activity changes between sign in and sing up created

--- app/src/main/java/com/example/herbert/stride/LoginActivity.java
+++ app/src/main/java/com/example/herbert/stride/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.herbert.stride;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView textViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textViewRegister = (TextView) findViewById(R.id.textViewRegister);
+
+        textViewRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == textViewRegister){
+            startActivity(new Intent(this, SignUpActivity.class));
+        }
     }
 }
